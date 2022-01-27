743ce0eea00683397dce18d5d956e098808b15cc
[M210®] <m210-2007@mail.ru>
2022-01-27 14:46:28 +0300

    Homework 2.6

--- src/ru/m210projects/Main.java
+++ src/ru/m210projects/Main.java
@@ -1,8 +1,73 @@
 package ru.m210projects;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
+    public static void task1() {
+        System.out.println("=============================== Task1");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        for (int i : nums) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("=============================== End of task1");
+    }
+
+    public static void task2() {
+        System.out.println("=============================== Task2");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        nums.sort(Comparator.naturalOrder());
+
+        int lastTyped = nums.get(0) - 1; // Гарантированное несуществующее число листа
+        for(int i : nums) {
+            if(i % 2 == 0 && i > lastTyped) {
+                System.out.println(i);
+                lastTyped = i;
+            }
+        }
+        System.out.println("=============================== End of task2");
+    }
+
+    public static void task3() {
+        System.out.println("=============================== Task3");
+        List<String> list = new ArrayList<String>(List.of("Hello", "hello", "World", "Word", "Mother", "Father", "father"));
+        list.replaceAll(e -> e.toLowerCase());
+        Set<String> set = new HashSet<String>(list);
+
+        System.out.println(set);
+
+        System.out.println("=============================== End of task3");
+    }
+
+    public static void task4() {
+        System.out.println("=============================== Task4");
+        List<String> list = new ArrayList<String>(List.of("Hello", "Father", "hello", "World", "Mother", "Word", "Mother", "Father", "father"));
+
+        list.replaceAll(e -> e.toLowerCase());
+        list.sort(Comparator.naturalOrder());
+
+        int doubles = 0;
+        for(int i = 0; i < list.size(); i++) {
+            String ref = list.get(i);
+            int j = i + 1;
+            while(j < list.size() && list.get(j++).equals(ref)) {
+                doubles++;
+                i++;
+            }
+        }
+
+        System.out.println("Words to remove: " + doubles);
+
+        System.out.println("=============================== End of task4");
     }
 }
